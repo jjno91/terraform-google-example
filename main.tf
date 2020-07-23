53c1abdcd092e602681b2bd11e8ae9cf20dba350
Jeremiah Norris <jjno91@gmail.com>
2020-07-23 17:18:41 -0500

    add project and k8s cluster

---
+++ main.tf
@@ -0,0 +1,41 @@
+resource "google_project" "this" {
+  name       = var.name
+  project_id = "${var.name}-${random_string.this.result}"
+}
+
+resource "random_string" "this" {
+  length  = 6
+  special = false
+}
+
+resource "google_container_cluster" "this" {
+  name                     = "this"
+  location                 = "us-central1-a"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "this" {
+  name       = "this"
+  location   = "us-central1-a"
+  cluster    = google_container_cluster.this.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "f1-micro"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
